5a1f2891a53a2b781ec4e8985030c99febc2de07
Lamont Sampson <148111503+lsampson1974@users.noreply.github.com>
2024-02-07 23:00:21 +0000

    Finishing up Rails Omnicalc 1

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -1,20 +1,93 @@
 class CalcController < ApplicationController
 
-    def squared_result
+    def squared
+
+        render ({ :template => "layouts/square"})
+
+    end
+
+  #------------------------------------------------------------
+
+    def square_result
   
-       @user_number = params[:user_number].to_f
+        @user_number = params[:user_number].to_f
 
-       @square = @user_number * @user_number
+        @square = @user_number * @user_number
 
-       render ({ :template => "layouts/square_result"})
+        render ({ :template => "layouts/square_result"})
   
     end # of method squared_root
 
-    def squared
+    #===========================================================
+  
+    def square_root
 
-       render ({ :template => "layouts/square"})
+        render ({ :template => "layouts/square_root"})
 
     end
+
+
+  #------------------------------------------------------------
+
+    def sqr_result
+
+        @user_number = params[:user_number].to_f
+
+        @square_root_of_number = Math.sqrt(@user_number)
+
+        render ({ :template => "layouts/sqr_result"})
+
+    end
+
+  #------------------------------------------------------------
+
+    def payment
+
+        render ({ :template => "layouts/payment"})
+
+    end
+
+  #------------------------------------------------------------
+
+    def payment_result
+
+      user_percentage = params[:user_apr]
+      percentage = user_percentage.to_f
+      interest_rate_period = percentage/100
+      interest_rate_period /= 12
   
+      user_percentage = user_percentage.to_f
+  
+      @display_percentage = user_percentage.to_fs(:percentage, {:precision => 4})
+  
+      @user_years = params[:user_years]
+      number_monthly_periods = @user_years.to_i*12
+  
+      user_principle = params[:user_pv]
+      principle = user_principle.to_f
+  
+      @display_principle = principle.to_fs(:currency)
+  
+      numerator = interest_rate_period * principle
+      denominator = 1 - (1 + interest_rate_period)**-(number_monthly_periods)
+  
+      payment = numerator / denominator
+  
+      @display_payment = payment.to_fs(:currency)
+  
+      render ({ :template => "layouts/payment_result"})
+
+    end
+
+  #------------------------------------------------------------
+
+  def payment
+
+    render ({ :template => "layouts/random"})
+
+  end
+
+  #------------------------------------------------------------
+
 
 end # Of class.
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,36 @@
 Rails.application.routes.draw do
+
+# SQUARED
+
+    get("/", { :controller => "calc", :action => "squared"})
+    get("/squared", { :controller => "calc", :action => "squared"})
+    get("/square_result", { :controller => "calc", :action => "square_result"})
+
+#-------------------------------------------------------------------------------------    
+
+# SQUARE ROOT
+
+    get("/square_root/new", { :controller => "calc", :action => "square_root"})
+    get("/sqr_result", { :controller => "calc", :action => "sqr_result"})
+
+#-------------------------------------------------------------------------------------    
   
+
+# PAYMENT
+
+get("/payment/new", { :controller => "calc", :action => "payment"})
+get("/payment_result", { :controller => "calc", :action => "payment_result"})
+
+#-------------------------------------------------------------------------------------    
+
+# RANDOM
+
+get("/random/new", { :controller => "calc", :action => "random"})
+get("/random_result", { :controller => "calc", :action => "random_result"})
+
+#-------------------------------------------------------------------------------------    
+  
+
+
+
 end
